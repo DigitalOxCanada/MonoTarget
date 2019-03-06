5292accdda0ca2fd14b02e7a364ab4128c22cd77
Perry @DigitalOx <perry@digitalox.ca>
2019-03-06 13:34:01 -0500

    Added Day7, HUD elements, SoundManager.

---
+++ Day7/SoundManager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoTarget
+{
+    public class SoundManager
+    {
+        private Game1 _game;
+        public float SoundVolume { get; set; }
+        public float MusicVolume { get; set; }
+        public float MasterVolume { get; set; }
+
+        public Dictionary<string, SoundEffect> SoundLibrary { get; set; } = new Dictionary<string, SoundEffect>();
+        public Dictionary<string, Song> MusicLibrary { get; set; } = new Dictionary<string, Song>();
+
+        public SoundManager(Game1 game)
+        {
+            _game = game;
+            SoundVolume = 1.0f;
+            MusicVolume = 1.0f;
+            MasterVolume = 1.0f;
+        }
+
+        public void SetSoundVolume(float v)
+        {
+            SoundVolume = v;
+        }
+
+        public void SetMusicVolume(float v)
+        {
+            MusicVolume = v;
+        }
+
+        public void SetMasterVolume(float v)
+        {
+            MasterVolume = v;
+            //overrides sound and music volume
+            MediaPlayer.Volume = v;
+        }
+
+        public void PlaySound(string nameOfSound, float volume)
+        {
+            var newvol = Math.Min(volume, SoundVolume);
+            newvol = Math.Min(newvol, MasterVolume);
+
+            SoundEffectInstance s;
+            s = SoundLibrary[nameOfSound].CreateInstance();
+            s.Volume = newvol;
+            s.Play();
+        }
+        public void PlayMusic(string nameOfSong, float volume)
+        {
+            var newvol = Math.Min(volume, SoundVolume);
+            newvol = Math.Min(newvol, MasterVolume);
+
+            MediaPlayer.Play(MusicLibrary[nameOfSong]);
+        }
+
+        internal void AddSound(string nameOfSound, string nameOfResource)
+        {
+            SoundLibrary.Add(nameOfSound, _game.Content.Load<SoundEffect>(nameOfResource));
+        }
+        internal void AddMusic(string nameOfSong, string nameOfResource)
+        {
+            MusicLibrary.Add(nameOfSong, _game.Content.Load<Song>(nameOfResource));
+        }
+    }
+}
---
+++ Day7/VisualLayers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoTarget
+{
+    /*
+ Layers from back to front
+ 1 - Background Layer 1
+ 2 - Background Layer 2
+ 3 - Objects Layer 1
+ 4 - Objects Layer 2
+ 5 - Objects Layer 3
+ 6 - Effects Layer 1
+ 7 - Effects Layer 2
+ 8 - HUD Layer 1
+ 9 - HUD Layer 2
+ 10 - Menu Layer 1
+ */
+    public static class VisualLayerID
+    {
+        public const int BG = 0;
+        public const int Objects = 2;
+        public const int HUD = 7;
+        public const int Menu = 9;
+    }
+
+    //public enum VisualLayerID : int
+    //{
+    //    BG = 0,
+    //    Objects = 2,
+    //    HUD = 7,
+    //    Menu = 9
+    //}
+}
